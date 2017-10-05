b0f5fc88ac88a05a65acd4bd4ab5569c3779778a
Jackie Blaum <jackie.blaum@gmail.com>
2017-10-05 09:09:47 -0700

    IC 443

--- scripts/get_slices.py
+++ scripts/get_slices.py
@@ -11,7 +11,11 @@ if __name__ == "__main__":
     parser.add_argument('--min', type=int, help='Minimum slice number', required=True)
     parser.add_argument('--max', type=int, help='Maximum slice number', required=True)
     parser.add_argument('--input', type=str, help='Input FITS file', required=True)
-
+    parser.add_argument('--sample', type=str, help='Sample file for WCS', required=True)
+    parser.add_argument('--path', type=str, help='Path to the directory where the output will be stored', required=True)
+    parser.add_argument('--threshold', type=int, help='Counts cutoff in order to trim the image', required=False, default=0)
+    parser.add_argument('--clean', type=bool, help='Do you want the image cleaned up?', required=False, default=True)
+    parser.add_argument('--radius', type=int, help='Largest radius of the main structure', required=False, default=1000)
     args = parser.parse_args()
 
-    make_slices(args.input, args.min, args.max)
+    make_slices(args.path, args.input, args.sample, args.min, args.max, args.clean, args.threshold, args.radius)
