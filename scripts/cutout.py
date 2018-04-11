60b4e7867ebc8036f291bab73a41ec1ebb4c546b
Jackie Blaum <jackie.blaum@gmail.com>
2018-04-11 13:17:45 -0700

    init

---
+++ scripts/cutout.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import argparse
+import os
+
+from ic443.ds9reg_cutout import ds9reg_cutout
+
+if __name__ == "__main__":
+
+    #Collect input
+    parser = argparse.ArgumentParser(description="Make DS9 region cutout from FITS file")
+    parser.add_argument('--infile', type=str, help='Input file', required=True)
+    parser.add_argument('--regname', type=float, help='Name of DS9 region (not including .reg)', required=True)
+    parser.add_argument('--binsize', type=str, help='Size of bins', required=False, default='0.025')
+    parser.add_argument('--outfile', type=str, help='Name of output file (not including .fits)', required=True)
+
+    args = parser.parse_args()
+
+    ds9reg_cutout(args.infile + '.fits', args.outfile, args.regname + '.reg', args.binsize)
